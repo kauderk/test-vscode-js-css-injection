627449eeb3a4b368430c04bdcdbff22d2a0d4123
kauderk <estudio1kau@gmail.com>
2023-12-18 03:03:23 -0500

    draft: attempt to refactor the main logic

--- src/extension.ts
+++ src/extension.ts
@@ -27,126 +27,8 @@ function activate(context: vscode.ExtensionContext) {
       `workbench.${uuid}.bak-custom-css`
     )
 
-  async function getContent(url: string) {
-    if (/^file:/.test(url)) {
-      const fp = fileURLToPath(url)
-      return await fs.promises.readFile(fp)
-    } else {
-      const response = await fetch(url)
-      return response.buffer()
-    }
-  }
-
-  // ####  main commands ######################################################
-
-  async function cmdInstall() {
-    const uuidSession = v4()
-    await createBackup(uuidSession)
-    await performPatch(uuidSession)
-  }
-
-  async function cmdReinstall() {
-    await uninstallImpl()
-    await cmdInstall()
-  }
-
-  async function cmdUninstall() {
-    await uninstallImpl()
-    disabledRestart()
-  }
-
-  async function uninstallImpl() {
-    const backupUuid = await getBackupUuid(htmlFile)
-    if (!backupUuid) return
-    const backupPath = BackupFilePath(backupUuid)
-    await restoreBackup(backupPath)
-    await deleteBackupFiles()
-  }
-
-  // #### Backup ################################################################
-
-  async function getBackupUuid(htmlFilePath: string) {
-    try {
-      const htmlContent = await fs.promises.readFile(htmlFilePath, 'utf-8')
-      const m = htmlContent.match(
-        /<!-- !! VSCODE-CUSTOM-CSS-SESSION-ID ([0-9a-fA-F-]+) !! -->/
-      )
-      if (!m) return null
-      else return m[1]
-    } catch (e) {
-      vscode.window.showInformationMessage(msg.somethingWrong + e)
-      throw e
-    }
-  }
-
-  async function createBackup(uuidSession: string) {
-    try {
-      let html = await fs.promises.readFile(htmlFile, 'utf-8')
-      html = clearExistingPatches(html)
-      await fs.promises.writeFile(BackupFilePath(uuidSession), html, 'utf-8')
-    } catch (e) {
-      vscode.window.showInformationMessage(msg.admin)
-      throw e
-    }
-  }
-
-  async function restoreBackup(backupFilePath: string) {
-    try {
-      if (fs.existsSync(backupFilePath)) {
-        await fs.promises.unlink(htmlFile)
-        await fs.promises.copyFile(backupFilePath, htmlFile)
-      }
-    } catch (e) {
-      vscode.window.showInformationMessage(msg.admin)
-      throw e
-    }
-  }
-
-  async function deleteBackupFiles() {
-    const htmlDir = path.dirname(htmlFile)
-    const htmlDirItems = await fs.promises.readdir(htmlDir)
-    for (const item of htmlDirItems) {
-      if (item.endsWith('.bak-custom-css')) {
-        await fs.promises.unlink(path.join(htmlDir, item))
-      }
-    }
-  }
-
   // #### Patching ##############################################################
 
-  async function performPatch(uuidSession: string) {
-    const config = vscode.workspace.getConfiguration('vscode_custom_css')
-    if (!patchIsProperlyConfigured(config)) {
-      return vscode.window.showInformationMessage(msg.notConfigured)
-    }
-
-    let html = await fs.promises.readFile(htmlFile, 'utf-8')
-    html = clearExistingPatches(html)
-
-    const injectHTML = await patchHtml(config)
-    html = html.replace(
-      /<meta\s+http-equiv="Content-Security-Policy"[\s\S]*?\/>/,
-      ''
-    )
-    let indicatorJS = ''
-    if (config.statusbar) indicatorJS = await getIndicatorJs()
-
-    html = html.replace(
-      /(<\/html>)/,
-      `<!-- !! VSCODE-CUSTOM-CSS-SESSION-ID ${uuidSession} !! -->\n` +
-        '<!-- !! VSCODE-CUSTOM-CSS-START !! -->\n' +
-        indicatorJS +
-        injectHTML +
-        '<!-- !! VSCODE-CUSTOM-CSS-END !! -->\n</html>'
-    )
-    try {
-      await fs.promises.writeFile(htmlFile, html, 'utf-8')
-    } catch (e) {
-      vscode.window.showInformationMessage(msg.admin)
-      disabledRestart()
-    }
-    enabledRestart()
-  }
   function clearExistingPatches(html: string) {
     html = html.replace(
       /<!-- !! VSCODE-CUSTOM-CSS-START !! -->[\s\S]*?<!-- !! VSCODE-CUSTOM-CSS-END !! -->\n*/,
@@ -157,57 +39,6 @@ function activate(context: vscode.ExtensionContext) {
       ''
     )
     return html
-  }
-
-  type Config = vscode.WorkspaceConfiguration
-  function patchIsProperlyConfigured(config: Config) {
-    return config && config.imports && config.imports instanceof Array
-  }
-
-  async function patchHtml(config: Config) {
-    let res = ''
-    for (const item of config.imports) {
-      const imp = await patchHtmlForItem(item)
-      if (imp) res += imp
-    }
-    return res
-  }
-  async function patchHtmlForItem(url: string) {
-    if (!url) return ''
-    if (typeof url !== 'string') return ''
-
-    // Copy the resource to a staging directory inside the extension dir
-    let parsed = new URL(url)
-    const ext = path.extname(parsed.pathname)
-
-    try {
-      const fetched = await getContent(url)
-      if (ext === '.css') {
-        return `<style>${fetched}</style>`
-      } else if (ext === '.js') {
-        return `<script>${fetched}</script>`
-      } else {
-        console.log(`Unsupported extension type: ${ext}`)
-      }
-    } catch (e) {
-      console.error(e)
-      vscode.window.showWarningMessage(msg.cannotLoad(url))
-      return ''
-    }
-  }
-  async function getIndicatorJs() {
-    let indicatorJsPath
-    let ext = vscode.extensions.getExtension('be5invis.vscode-custom-css')
-    if (ext && ext.extensionPath) {
-      indicatorJsPath = path.resolve(ext.extensionPath, 'src/statusbar.js')
-    } else {
-      indicatorJsPath = path.resolve(__dirname, 'statusbar.js')
-    }
-    const indicatorJsContent = await fs.promises.readFile(
-      indicatorJsPath,
-      'utf-8'
-    )
-    return `<script>${indicatorJsContent}</script>`
   }
 
   function reloadWindow() {
@@ -225,6 +56,112 @@ function activate(context: vscode.ExtensionContext) {
       .then(reloadWindow)
   }
 
+  // ####  main commands ######################################################
+
+  async function cmdInstall() {
+    const uuidSession = v4()
+    // await createBackup(uuidSession)
+    {
+      try {
+        let html = await fs.promises.readFile(htmlFile, 'utf-8')
+        html = clearExistingPatches(html)
+        await fs.promises.writeFile(BackupFilePath(uuidSession), html, 'utf-8')
+      } catch (e) {
+        vscode.window.showInformationMessage(msg.admin)
+        throw e
+      }
+    }
+    // await performPatch(uuidSession)
+    {
+      let html = await fs.promises.readFile(htmlFile, 'utf-8')
+      html = clearExistingPatches(html)
+
+      html = html.replace(
+        /<meta\s+http-equiv="Content-Security-Policy"[\s\S]*?\/>/,
+        ''
+      )
+
+      let indicatorJsPath
+      let ext = vscode.extensions.getExtension('be5invis.vscode-custom-css')
+      if (ext && ext.extensionPath) {
+        indicatorJsPath = path.resolve(ext.extensionPath, 'src/statusbar.js')
+      } else {
+        indicatorJsPath = path.resolve(__dirname, 'statusbar.js')
+      }
+      const indicatorJsContent = await fs.promises.readFile(
+        indicatorJsPath,
+        'utf-8'
+      )
+      const indicatorJS = `<script>${indicatorJsContent}</script>`
+
+      html = html.replace(
+        /(<\/html>)/,
+        `<!-- !! VSCODE-CUSTOM-CSS-SESSION-ID ${uuidSession} !! -->\n` +
+          '<!-- !! VSCODE-CUSTOM-CSS-START !! -->\n' +
+          indicatorJS +
+          '<!-- !! VSCODE-CUSTOM-CSS-END !! -->\n</html>'
+      )
+      try {
+        await fs.promises.writeFile(htmlFile, html, 'utf-8')
+      } catch (e) {
+        vscode.window.showInformationMessage(msg.admin)
+        disabledRestart()
+      }
+      enabledRestart()
+    }
+  }
+
+  async function cmdReinstall() {
+    await uninstallImpl()
+    await cmdInstall()
+  }
+
+  async function cmdUninstall() {
+    await uninstallImpl()
+    disabledRestart()
+  }
+
+  async function uninstallImpl() {
+    // if typescript wont won't freak out about promises then nothing matters :D
+    // getBackupUuid
+    const backupUuid = await (async function getBackupUuid() {
+      try {
+        const htmlContent = await fs.promises.readFile(htmlFile, 'utf-8')
+        const m = htmlContent.match(
+          /<!-- !! VSCODE-CUSTOM-CSS-SESSION-ID ([0-9a-fA-F-]+) !! -->/
+        )
+        if (!m) throw new Error('no backup uuid found')
+        else return m[1]
+      } catch (e) {
+        vscode.window.showInformationMessage(msg.somethingWrong + e)
+        throw e
+      }
+    })()
+
+    // restoreBackup
+    const backupFilePath = BackupFilePath(backupUuid)
+    {
+      try {
+        if (fs.existsSync(backupFilePath)) {
+          await fs.promises.unlink(htmlFile)
+          await fs.promises.copyFile(backupFilePath, htmlFile)
+        }
+      } catch (e) {
+        vscode.window.showInformationMessage(msg.admin)
+        throw e
+      }
+    }
+    // deleteBackupFiles
+    {
+      const htmlDir = path.dirname(htmlFile)
+      const htmlDirItems = await fs.promises.readdir(htmlDir)
+      for (const item of htmlDirItems) {
+        if (item.endsWith('.bak-custom-css')) {
+          await fs.promises.unlink(path.join(htmlDir, item))
+        }
+      }
+    }
+  }
   const installCustomCSS = vscode.commands.registerCommand(
     'extension.installCustomCSS',
     cmdInstall
